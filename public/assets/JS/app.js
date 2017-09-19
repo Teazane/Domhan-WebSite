5b8b55d88a39264d9da8c13d6baad6020614ca31
Leveque Lucile <lucile.leveque@insa-rennes.fr>
2017-09-19 04:28:41 +0200

    Mise en place de la navigation + page accueil

--- public/assets/JS/app.js
+++ public/assets/JS/app.js
@@ -1,12 +1,18 @@
-var app = angular.module("domhanWebSite", ["ngRoute", "ngMap"]);
+var app = angular.module("domhanWebSite", ["ngRoute"]);
 
 app.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(false).hashPrefix('!');
 
     //Gestion du routing
     $routeProvider
-    .when("/home", { //Quand cette URL est appelée
-        templateUrl : "pages/home.html", //La page correspondante est affichée
-        controller: "HomeController" //Et le controlleur approprié est appelé
+    .when("/", { //Quand cette URL est appelée
+      templateUrl : "pages/home.html", //La page correspondante est affichée
+    })
+    .when("/truc_autre", {
+      templateUrl : "pages/truc_autre.html", //La page correspondante est affichée
+      controller: "TrucController" //Et le controlleur approprié est appelé
+    })
+    .otherwise( {
+      redirectTo: "/"
     });
 });
